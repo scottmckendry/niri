59d14398bc1ed33d03d472495df3535f0e6e5226
Scott McKendry <me@scottmckendry.tech>
2026-03-14 19:38:17 +1300

    flake

--- flake.nix
+++ flake.nix
@@ -72,6 +72,9 @@
             # NOTE: This is only used for Git dependencies
             allowBuiltinFetchGit = true;
             lockFile = ./Cargo.lock;
+            outputHashes = {
+              "smithay-0.7.0" = "sha256-89LEQ9M1YbJ2Jjg2lv3XLWGGaYIuNToMSGb8jWMX/cY=";
+            };
           };
 
           strictDeps = true;
@@ -82,23 +85,22 @@
             installShellFiles
           ];
 
-          buildInputs =
-            [
-              cairo
-              dbus
-              libGL
-              libdisplay-info
-              libinput
-              seatd
-              libxkbcommon
-              libgbm
-              pango
-              wayland
-            ]
-            ++ lib.optional (withDbus || withScreencastSupport || withSystemd) dbus
-            ++ lib.optional withScreencastSupport pipewire
-            # Also includes libudev
-            ++ lib.optional withSystemd systemd;
+          buildInputs = [
+            cairo
+            dbus
+            libGL
+            libdisplay-info
+            libinput
+            seatd
+            libxkbcommon
+            libgbm
+            pango
+            wayland
+          ]
+          ++ lib.optional (withDbus || withScreencastSupport || withSystemd) dbus
+          ++ lib.optional withScreencastSupport pipewire
+          # Also includes libudev
+          ++ lib.optional withSystemd systemd;
 
           buildFeatures =
             lib.optional withDbus "dbus"
@@ -122,21 +124,20 @@
             "--skip=::egl"
           ];
 
-          postInstall =
-            ''
-              installShellCompletion --cmd niri \
-                --bash <($out/bin/niri completions bash) \
-                --fish <($out/bin/niri completions fish) \
-                --nushell <($out/bin/niri completions nushell) \
-                --zsh <($out/bin/niri completions zsh)
+          postInstall = ''
+            installShellCompletion --cmd niri \
+              --bash <($out/bin/niri completions bash) \
+              --fish <($out/bin/niri completions fish) \
+              --nushell <($out/bin/niri completions nushell) \
+              --zsh <($out/bin/niri completions zsh)
 
-              install -Dm644 resources/niri.desktop -t $out/share/wayland-sessions
-              install -Dm644 resources/niri-portals.conf -t $out/share/xdg-desktop-portal
-            ''
-            + lib.optionalString withSystemd ''
-              install -Dm755 resources/niri-session $out/bin/niri-session
-              install -Dm644 resources/niri{.service,-shutdown.target} -t $out/share/systemd/user
-            '';
+            install -Dm644 resources/niri.desktop -t $out/share/wayland-sessions
+            install -Dm644 resources/niri-portals.conf -t $out/share/xdg-desktop-portal
+          ''
+          + lib.optionalString withSystemd ''
+            install -Dm755 resources/niri-session $out/bin/niri-session
+            install -Dm644 resources/niri{.service,-shutdown.target} -t $out/share/systemd/user
+          '';
 
           env = {
             # Force linking with libEGL and libwayland-client
